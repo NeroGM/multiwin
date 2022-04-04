a54ae044e2ae414a77054aa68a92f0d10ee28fea
NeroGM <102484529+NeroGM@users.noreply.github.com>
2022-04-04 13:32:56 +0000

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,8 +1,10 @@
+package;
+
 class Main extends hxd.App {
     var txt1:h2d.Text;
     var txt2:h2d.Text;
     var c:Float = 0;
-    var b:Bool = false;
+    var b:Int = 0;
     override function init() {
         txt1 = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
         txt1.text = "WINDOW 1 !";
@@ -12,16 +14,26 @@ class Main extends hxd.App {
         txt2 = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
         txt2.text = "POS : ";
         txt2.setScale(2);
+        @:privateAccess engine.window.title = "1";
     }
 
     override function update(dt:Float) {
-        txt2.text = "X: " + s2d.mouseX + " Y:" + s2d.mouseY;
+        txt2.text = "X: " + s2d.mouseX + " Y:" + s2d.mouseY + " | " + engine.fps ;
         txt1.rotate(0.025);
         c+=dt;
-        if (!b && c >= 1) {
+
+        if (b < 1 && c >= 2) {
             new Main2();
-            b = true;
+            c = 0;
+            b++;
         }
+
+        // if (b < 2 && c >= 2) {
+        //     new Main3();
+        //     c = 0;
+        // }
+
+        // if (c > 5) Sys.exit(0);
     }
     static function main() {
         new Main();
